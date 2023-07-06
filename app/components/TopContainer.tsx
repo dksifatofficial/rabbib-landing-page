01e45b6a2890bacb51ef36b989b1a0a8ea3e51fd
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-07-06 14:54:53 +0600

    res

--- app/components/TopContainer.tsx
+++ app/components/TopContainer.tsx
@@ -13,8 +13,8 @@ function TopContainer() {
       alt='heropic'
       />
       <div className=" absolute justify-self-center grid justify-items-center top-[60px]">
-        <h1 className="text-white text-6xl p-4 font-bold">I&apos;m Startup</h1>
-        <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+        <h1 className="text-white text-4xl p-4 font-bold lg:text-6xl">I&apos;m Startup</h1>
+        <p className="text-white px-[20px] text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
         <p className="uppercase text-white px-8 py-2 bg-blue-500 rounded-[20px] mt-6">Best service for you</p>
       </div>
     </div>
