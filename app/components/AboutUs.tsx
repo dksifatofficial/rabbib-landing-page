6f40aec0edab91f989cec82b9eb28d848032f459
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-07-06 12:38:26 +0600

    res

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -4,21 +4,17 @@ import Box from "../components/Box";
 function AboutUs() {
   return (
     <div className="w-full h-auto mt-[100px]" id="aboutMe">
-      <div className=" w-full mx-[10px] h-[280px] mb-[20px] rounded-[10px] bg-[#000] grid justify-items-center 
-      lg:h-[380px] lg:mx-[50px] lg:mb-0 lg:bg-[#2d2e2d] lg:w-auto">
+      <div className=" mx-[50px] h-[380px] rounded-[10px] bg-[#2d2e2d] flex justify-center">
         <h1
-          className="px-[100px] py-[10px] rounded-[10px] text-4xl font-semibold 
-         mt-[90px] bg-[#fff] text-[#000]
-          lg:px-[100px] lg:bg-black lg:text-white lg:absolute"
+          className=" absolute px-[100px] py-[10px] rounded-[10px] text-4xl font-semibold 
+         mt-[90px] bg-black text-white"
         >
           About Us
         </h1>
       </div>
       <div className="w-full h-auto bg-black flex justify-center">
-        <div
-          className="w-[90%] h-auto bg-transparent flex flex-wrap justify-center 
-        gap-5 gap-y-5 gap-x-5 mb-20 lg:mt-[-150px] lg:mb-[120px]"
-        >
+        <div className="w-[90%] h-auto bg-transparent flex flex-wrap justify-center 
+        gap-5 gap-y-5 gap-x-5 mb-20 lg:mt-[-150px] lg:mb-[120px]">
           <Box className="bg-[#2d2e2d] hover:bg-[#444644]">
             <div>
               <FaHandshake className="text-[50px] text-[#000] bg-white p-2.5 rounded-[100%]" />
